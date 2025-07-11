427e867f194da693ca6b8234adde629720a8144b
Prerith Subramanya <prerithsubramanya@gmail.com>
2025-07-11 18:43:31 +1000

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// TODO: Replace with your app's Firebase project configuration.
+// This file should be deployed with your public-facing auth page.
+const firebaseConfig = {
+      apiKey: 'AIzaSyCS8Gj1fmeUg00x3poDtmJ3V_SPIt1MbLk',
+      authDomain: 'teleparty-extension.firebaseapp.com',
+      projectId: 'teleparty-extension',
+      storageBucket: 'teleparty-extension.firebasestorage.app',
+      messagingSenderId: '1179597965613',
+      appId: '1:179597965613:web:5254107ae63c8e2bed7ba4',
+};
+
+export default firebaseConfig;
---
+++ signInWithPopup.js
@@ -0,0 +1,20 @@
+import { signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const PARENT_FRAME = document.location.ancestorOrigins[0];
+const PROVIDER = new GoogleAuthProvider();
+
+function sendResponse(result) {
+  globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
+}
+
+globalThis.addEventListener('message', function({data}) {
+  if (data.initAuth) {
+    signInWithPopup(auth, PROVIDER)
+      .then(sendResponse)
+      .catch(sendResponse)
+  }
+});
